18823bdab6bce46f950189ad82c04272c3b29596
Renato <paulo.borupdebakker@dci-student.org>
2025-09-10 23:36:52 +0200

    header ready

--- portifolio/src/app/layout.tsx
+++ portifolio/src/app/layout.tsx
@@ -1,7 +1,15 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/Header";
+import { Permanent_Marker } from "next/font/google";
 
+
+const permanentMarker = Permanent_Marker({
+  subsets: ["latin"],
+  weight: "400", 
+  variable: "--font-permanent-marker",
+});
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -25,8 +33,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} ${permanentMarker.variable} antialiased`}
       >
+        <Header />
         {children}
       </body>
     </html>
--- portifolio/src/components/Header.tsx
+++ portifolio/src/components/Header.tsx
@@ -1,14 +1,35 @@
-import React from 'react'
+import React from 'react';
+import Link from 'next/link';
+import { CiLinkedin } from "react-icons/ci";
+import { FaGithub } from "react-icons/fa";
+import { FaXing } from "react-icons/fa";
+
 
 function Header() {
   return (
-    <header className='flex w-full h-[12rem]'>
-      <nav>
-        <ul>
-            <li></li>
-            <li></li>
-        </ul>
-      </nav>
+    <header className='flex flex-row w-full h-[5rem] bg-gradient-to-br from-stone-900 to-sky-500 items-center *:p-3 '>
+        <nav className='flex flex-row justify-between w-full items-centerbvbh'>
+            <h1 className="font-permanent_marker text-5xl bg-gradient-to-r from-pink-500 via-purple-500 to-blue-500 bg-[length:200%_200%] bg-clip-text text-transparent animate-gradient">Portifolio</h1>
+
+            <ul className='flex flex-row *:m-2'>
+                <li><Link href="/" className="font-mono">Home</Link></li>
+                <li><Link href="/projects" className="font-mono">Projects</Link></li>
+                <li><Link href="/contact" className="font-mono">Contact</Link></li>
+            </ul>
+            <nav>
+                <ul className='flex flex-row *:m-2 text-white'>
+                    <li>
+                        <Link href="https://www.linkedin.com/in/renato-de-bakker-400543360/" target='_blank'><CiLinkedin /></Link>
+                    </li>
+                    <li>
+                        <Link href="https://github.com/P-Renato" target='_blank'><FaGithub /></Link>
+                    </li>
+                    <li>
+                        <Link href="https://www.xing.com/profile/Renato_deBakker/web_profiles" target='_blank'><FaXing /></Link>
+                    </li>
+                </ul>
+            </nav>
+        </nav>
     </header>
   )
 }
