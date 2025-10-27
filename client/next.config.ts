3e343c2097e7df8be32f8c8faf3842a9743165a1
Renato <paulo.borupdebakker@dci-student.org>
2025-10-27 23:06:46 +0100

    changed next.config.ts

--- client/next.config.ts
+++ client/next.config.ts
@@ -2,11 +2,24 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export", // ✅ Tells Next.js to produce static HTML files
-   outputFileTracingRoot: __dirname,
+  outputFileTracingRoot: __dirname,
   images: {
     domains: ["github.com", "user-images.githubusercontent.com"],
     unoptimized: true, // ✅ Needed when using static export (no image optimizer)
   },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "default-src 'self'; connect-src 'self' https://renato-portifolio-backend.onrender.com; style-src 'self' 'unsafe-inline';"
+          },
+        ],
+      },
+    ];
+  }
 };
 
 export default nextConfig;
