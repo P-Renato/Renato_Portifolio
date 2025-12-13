4755bbbde741becd1c20ce4c6f3f858cd208b58c
Renato <paulo.borupdebakker@dci-student.org>
2025-12-13 04:12:44 +0100

    route fixed

--- client/src/app/contact/page.tsx
+++ client/src/app/contact/page.tsx
@@ -38,9 +38,11 @@ export default function ContactPage() {
   });
 
   async function onSubmit(values: z.infer<typeof messageFormSchema>) {
+    const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5001';
+
 
     try {
-      const response = await fetch("/api/contact", {
+      const response = await fetch(API_BASE_URL, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",  
